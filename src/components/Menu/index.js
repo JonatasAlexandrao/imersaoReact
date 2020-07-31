dd7920ad25ba52a98daf16a86255edd25f4817f4
Jônatas <jonatas-flash@hotmail.com>
2020-07-31 10:08:24 -0300

    aula 3 formulario

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/JonFlix.png'
 import './Menu.css'
-import ButtonLink from './ButtonLink';
 import Button from '../Button';
 
 function Menu() {
   return (
     <nav className='Menu'>
-      <a href='/'><img className='Logo' src={Logo} alt='Jonflix Logo'/></a>
+      <Link to='/'>
+        <img className='Logo' src={Logo} alt='Jonflix Logo'/>
+      </Link>
       
-      <Button className='ButtonLink' href='/'>Novo Vídeo</Button>
+      <Button as={Link} className='ButtonLink' to='/cadastro/video'>Novo Vídeo</Button>
     </nav>
   );
 }
